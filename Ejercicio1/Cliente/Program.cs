9dbaf0f53e01a0452771eca4d2cca4949a89d292
nuriarosell <nuriarosell1999@gmail.com>
2025-04-24 17:48:06 +0200

    Etapa 1

---
+++ Ejercicio1/Cliente/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading;
+using NetworkStreamNS;
+using CarreteraClass;
+using VehiculoClass;
+
+namespace Client
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Definir la dirección IP del servidor y el puerto
+            string direccionIP = "127.0.0.1"; // localhost
+            int puerto = 5000;
+
+            // Crear un objeto TcpClient y conectarse al servidor
+            TcpClient cliente = new TcpClient(direccionIP, puerto);
+            Console.WriteLine("Conectado al servidor.");
+
+            // Obtener el flujo de datos del servidor
+            NetworkStream stream = cliente.GetStream();
+
+            // Leer los datos enviados por el servidor
+            byte[] datos = new byte[256];
+            int bytesLeidos = stream.Read(datos, 0, datos.Length);
+            string mensaje = Encoding.ASCII.GetString(datos, 0, bytesLeidos);
+
+            // Mostrar el mensaje recibido
+            Console.WriteLine($"Mensaje del servidor: {mensaje}");
+
+            // Cerrar la conexión
+            cliente.Close();
+
+        }
+
+    }
+}
---
+++ Ejercicio1/Servidor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading;
+using NetworkStreamNS;
+using CarreteraClass;
+using VehiculoClass;
+
+namespace Servidor
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {      
+            // Definir el puerto y la dirección IP
+            int puerto = 5000;
+            string direccionIP = "127.0.0.1"; // localhost
+
+            // Crear un objeto de TcpListener que escuche en la dirección IP y el puerto
+            TcpListener servidor = new TcpListener(IPAddress.Parse(direccionIP), puerto);
+            servidor.Start();
+
+            Console.WriteLine("Servidor esperando conexiones...");
+
+            // Aceptar conexiones de clientes
+            while (true)
+            {
+                // Aceptar la conexión de un cliente
+                TcpClient cliente = servidor.AcceptTcpClient();
+                Console.WriteLine("Cliente conectado.");
+
+                // Obtener el flujo de datos del cliente
+                NetworkStream stream = cliente.GetStream();
+                
+                // Enviar un mensaje al cliente
+                byte[] mensaje = Encoding.ASCII.GetBytes("Bienvenido al servidor.");
+                stream.Write(mensaje, 0, mensaje.Length);
+                
+                // Cerrar la conexión con el cliente
+                cliente.Close();
+            }      
+
+
+        }
+    }
+}
+
